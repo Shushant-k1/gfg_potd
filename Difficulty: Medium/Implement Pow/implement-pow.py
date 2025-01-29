c3991c56b58b426127f293519b1ad16f233266cb
shushant kumar <rekhaubgb@gmail.com>
2025-01-29 21:53:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Pow/implement-pow.py
@@ -0,0 +1,28 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+import math
+class Solution:
+    def power(self, b: float, e: int) -> float:
+        # Code Here
+        return math.pow(b , e)
+        
+
+#{ 
+ # Driver Code Starts.
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        b = float(input())
+        e = int(input())
+        ob = Solution()
+        result = ob.power(b, e)
+        print(f"{result:.5f}")
+        print("~")
+# } Driver Code Ends
